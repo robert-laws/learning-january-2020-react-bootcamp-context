e6173fd036a7ee4e624f42e647d47679f153f9d7
Robert Laws <roblaws@me.com>
2020-01-27 17:03:57 +0300

    begin adding form

---
+++ src/Form.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlinedIcon';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import { withStyles } from '@material-ui/core/styles/withStyles';
+import styles from './styles/FormStyles';
+
+const Form = () => {
+  return (
+    <main className={classes.main}>
+
+    </main>
+  )
+}
+
+export default withStyles(styles)(Form);
---
+++ src/styles/FormStyles.js
@@ -0,0 +1,13 @@
+const styles = theme => ({
+  main: {
+    width: 'auto',
+    display: 'block',
+    marginLeft: theme.spacing(3),
+    marginRight: theme.spacing(3),
+    [theme.breakpoints.up('md')]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto'
+    }
+  }
+})
